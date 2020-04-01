cdd0976e82aa000175d432414252c5a28823042b
Lucas Holder <lucasholderx@gmail.com>
2020-04-01 02:19:51 -0400

    update README / ci

--- README.md
+++ README.md
@@ -21,38 +21,52 @@ If you have Rust:
 cargo install fair
 ```
 
-## Usage
+## Example
 
 ```bash
-fair <client_seed> <server_seed> <nonce> <game>
-```
-
-Example usage:
-
-```bash
-$ fair "client seed" "server seed" 2 baccarat
+$ fair baccarat "client seed" "server seed" 1  master ✗
 Client seed: client seed
 Server seed: server seed
-Nonce: 2
+Nonce: 1
 
 Player won
 
-Player (9): ♦9 - ♦10
-Banker (7): ♥4 - ♦3
+Player (9): ♠J - ♥10 - ♥9
+Banker (5): ♥5 - ♣K
+```
 
-$ fair plinko --help
-Plinko game
+## Usage
+
+```bash
+$ fair --help
+fair 0.0.5-alpha.0
+Lucas Holder <lucasholderx@gmail.com>
+CLI tool and library for verifying provably fair games (baccarat, etc.).
 
 USAGE:
-    fair <client_seed> <server_seed> <nonce> plinko [OPTIONS]
+    fair [SUBCOMMAND]
 
 FLAGS:
     -h, --help       Prints help information
     -V, --version    Prints version information
 
-OPTIONS:
-        --risk <risk>    Risk [possible values: low, medium, high]
-        --rows <rows>    Rows
+SUBCOMMANDS:
+    baccarat         Baccarat game
+    blackjack        Blackjack
+    crash            Crash game (uses Stake.com's parameters). Does not use client/server seed and nonce arguments.
+    diamond_poker    Diamond poker
+    dice             Dice game
+    help             Prints this message or the help of the given subcommand(s)
+    hilo             Hilo game
+    keno             Keno
+    limbo            Limbo game
+    mines            Mines game
+    plinko           Plinko game
+    roulette         Roulette
+    slots            Slots game(s)
+    video_poker      Video Poker
+    wheel            Wheel game
+
 ```
 
 As expected, we get the same result as on
--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,53 @@ fn main() {
         (author: crate_authors!())
         (about: crate_description!())
         (@subcommand baccarat =>
-            (about: "Baccarat game"))
+            (about: "Baccarat game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
+        )
         (@subcommand dice =>
             (about: "Dice game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand limbo =>
             (about: "Limbo game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand hilo =>
             (about: "Hilo game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand blackjack =>
             (about: "Blackjack")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand diamond_poker =>
             (about: "Diamond poker")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand roulette =>
             (about: "Roulette")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand plinko =>
             (about: "Plinko game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
+
             (@arg risk: --risk +takes_value
                  default_value("low")
                  possible_value[low]
@@ -52,6 +78,10 @@ fn main() {
         )
         (@subcommand mines =>
             (about: "Mines game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
+
             (@arg mines: --mines +takes_value
                  default_value("3")
                  {validate_mines_mines}
@@ -59,9 +89,16 @@ fn main() {
         )
         (@subcommand video_poker =>
             (about: "Video Poker")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
         )
         (@subcommand wheel =>
             (about: "Wheel game")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
+
             (@arg risk: --risk +takes_value
                  default_value("low")
                  possible_value[low]
@@ -79,6 +116,10 @@ fn main() {
         )
         (@subcommand slots =>
             (about: "Slots game(s)")
+            (@arg client_seed: +required "Client seed")
+            (@arg server_seed: +required "Server seed")
+            (@arg nonce: +required "Nonce (positive integer)")
+
             (@arg round: --round +takes_value
                  default_value("0")
                  "Round #")
@@ -90,68 +131,14 @@ fn main() {
                  {validate_hex}
                  "Game hash")
         )
-        (@arg client_seed: +required "Client seed")
-        (@arg server_seed: +required "Server seed")
-        (@arg nonce: +required "Nonce (positive integer)")
     )
     .get_matches();
 
-    // let game = matches.value_of("game").unwrap().to_lowercase();
-    let client_seed = matches.value_of("client_seed").unwrap();
-    let server_seed = matches.value_of("server_seed").unwrap();
-    // println!("{:?}", matches);
+    let (game, maybe_sub_matches) = matches.subcommand();
+    let sub_matches = maybe_sub_matches.unwrap();
 
-    // TODO: list supported games!
-    // TODO use value_t! to parse game.. ensure game is in valid list of strings...
-    let nonce: u64 = value_t!(matches, "nonce", u64).unwrap_or_else(|e| e.exit());
-    // println!("{:?}", matches);
-    println!("Client seed: {}", client_seed);
-    println!("Server seed: {}", server_seed);
-    println!("Nonce: {}", nonce);
-    println!("");
-
-    let config = ProvablyFairConfig::new(client_seed, server_seed, nonce);
-
-    let res = match matches.subcommand() {
-        ("baccarat", _) => baccarat::simulate(config).to_string(),
-        ("dice", _) => dice::simulate(config).to_string(),
-        ("limbo", _) => limbo::simulate(config).to_string(),
-        ("hilo", _) => hilo::simulate(config).to_string(),
-        ("blackjack", _) => blackjack::simulate(config).to_string(),
-        ("diamond_poker", _) => diamond_poker::simulate(config).to_string(),
-        ("roulette", _) => roulette::simulate(config).to_string(),
-        ("plinko", Some(sub_matches)) => {
-            let rows: u8 = value_t!(sub_matches, "rows", u8).unwrap_or_else(|e| e.exit());
-            let risk = sub_matches.value_of("risk").unwrap_or("low");
-
-            let risk = match risk {
-                "low" => plinko::Risk::Low,
-                "medium" => plinko::Risk::Medium,
-                "high" => plinko::Risk::High,
-                _ => panic!("Uknown risk {}", risk),
-            };
-
-            let opts = plinko::Opts::new(rows, risk);
-            plinko::simulate(config, Some(opts)).to_string()
-        }
-        ("keno", _) => keno::simulate(config).to_string(),
-        ("mines", Some(sub_matches)) => {
-            let mines: u8 = value_t!(sub_matches, "mines", u8).unwrap_or_else(|e| e.exit());
-            mines::simulate(config, mines).to_string()
-        }
-        ("video_poker", _) => video_poker::simulate(config).to_string(),
-        ("wheel", Some(sub_matches)) => {
-            let segments: u8 = value_t!(sub_matches, "segments", u8).unwrap_or_else(|e| e.exit());
-            let risk = sub_matches.value_of("risk").unwrap_or("low");
-            let risk = wheel::Risk::from_str(risk);
-            let opts = wheel::Opts::new(segments, risk);
-            wheel::simulate(config, Some(opts)).to_string()
-        }
-        ("slots", Some(sub_matches)) => {
-            let round: usize = value_t!(sub_matches, "round", usize).unwrap_or_else(|e| e.exit());
-            slots::simulate(config, round).to_string()
-        }
-        ("crash", Some(sub_matches)) => {
+    match game {
+        "crash" => {
             // TODO: ensure game_hash is valid hex with hex::decode
             let game_hash = sub_matches.value_of("game_hash").unwrap();
 
@@ -169,11 +156,69 @@ fn main() {
                 println!("");
                 println!("IMPORTANT: use --verify to verify the game hash is valid");
             }
-            "".to_string()
         }
-        _ => die("This branch should never execute. Unimplemented game?"),
+        _ => {
+            // let game = matches.value_of("game").unwrap().to_lowercase();
+            let client_seed = sub_matches.value_of("client_seed").unwrap();
+            let server_seed = sub_matches.value_of("server_seed").unwrap();
+            // println!("{:?}", matches);
+
+            // TODO: list supported games!
+            // TODO use value_t! to parse game.. ensure game is in valid list of strings...
+            let nonce: u64 = value_t!(sub_matches, "nonce", u64).unwrap_or_else(|e| e.exit());
+            // println!("{:?}", matches);
+            println!("Client seed: {}", client_seed);
+            println!("Server seed: {}", server_seed);
+            println!("Nonce: {}", nonce);
+            println!("");
+
+            let config = ProvablyFairConfig::new(client_seed, server_seed, nonce);
+            let res = match game {
+                "baccarat" => baccarat::simulate(config).to_string(),
+                "dice" => dice::simulate(config).to_string(),
+                "limbo" => limbo::simulate(config).to_string(),
+                "hilo" => hilo::simulate(config).to_string(),
+                "blackjack" => blackjack::simulate(config).to_string(),
+                "diamond_poker" => diamond_poker::simulate(config).to_string(),
+                "roulette" => roulette::simulate(config).to_string(),
+                "plinko" => {
+                    let rows: u8 = value_t!(sub_matches, "rows", u8).unwrap_or_else(|e| e.exit());
+                    let risk = sub_matches.value_of("risk").unwrap_or("low");
+
+                    let risk = match risk {
+                        "low" => plinko::Risk::Low,
+                        "medium" => plinko::Risk::Medium,
+                        "high" => plinko::Risk::High,
+                        _ => panic!("Uknown risk {}", risk),
+                    };
+
+                    let opts = plinko::Opts::new(rows, risk);
+                    plinko::simulate(config, Some(opts)).to_string()
+                }
+                "keno" => keno::simulate(config).to_string(),
+                "mines" => {
+                    let mines: u8 = value_t!(sub_matches, "mines", u8).unwrap_or_else(|e| e.exit());
+                    mines::simulate(config, mines).to_string()
+                }
+                "video_poker" => video_poker::simulate(config).to_string(),
+                "wheel" => {
+                    let segments: u8 =
+                        value_t!(sub_matches, "segments", u8).unwrap_or_else(|e| e.exit());
+                    let risk = sub_matches.value_of("risk").unwrap_or("low");
+                    let risk = wheel::Risk::from_str(risk);
+                    let opts = wheel::Opts::new(segments, risk);
+                    wheel::simulate(config, Some(opts)).to_string()
+                }
+                "slots" => {
+                    let round: usize =
+                        value_t!(sub_matches, "round", usize).unwrap_or_else(|e| e.exit());
+                    slots::simulate(config, round).to_string()
+                }
+                _ => die("This branch should never execute. Unimplemented game?"),
+            };
+            println!("{}", res);
+        }
     };
-    println!("{}", res);
 }
 
 fn validate_plinko_rows(rows: String) -> std::result::Result<(), String> {
